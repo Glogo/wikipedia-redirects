dee4d8d8c43153185e9c8ca7135f807abe4d6930
Michael Gloger <gloger.michael@gmail.com>
2014-11-18 18:38:12 +0100

    Updated samples & unit test

--- parser/src/com/glogo/wikiparser/tests/WikiParserTest.java
+++ parser/src/com/glogo/wikiparser/tests/WikiParserTest.java
@@ -17,61 +17,74 @@ import org.junit.Test;
 import com.glogo.wikiparser.WikipediaRedirects;
 
 /**
- * This class contains simple jUnit test for WikiParser.
- * For testing is used input data file named "test_input.xml"
+ * This class contains simple jUnit tests for WikiParser.
+ * For testing is used input data file named "sample_input_enwiki-latest-pages-articles1_alt.xml" and output is saved in user home directory
  * For detailed information please see <a href="https://github.com/Glogo/wikipedia-redirects">project repository</a>
  * @author michael.gloger
  */
 public class WikiParserTest {
 	
+	private static final String INPUT_FILENAME = "sample_input_enwiki-latest-pages-articles1_alt.xml";
+	private static final String OUTPUT_FILENAME = "sample_output_enwiki-latest-pages-articles1_alt.csv";
 	private static WikipediaRedirects parser = null;
-	private static String absolutePath;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		
+		// Initialize parser
 		parser = new WikipediaRedirects();
 		
-		URL testInputUrl = WikiParserTest.class.getClassLoader().getResource("test_input.xml");
+		// Get absolute input resource url
+		URL testInputUrl = WikiParserTest.class.getClassLoader().getResource(INPUT_FILENAME);
 		
-		// Decode url to remove %20 space sepparators
-		absolutePath = URLDecoder.decode(testInputUrl.getFile(), "UTF-8");
-	}
-	
-	@Test
-	public void testFileShouldExist() {
-		assertTrue("Test file could not be read.", absolutePath != null || absolutePath.length() != 0);
+		// Check if file exists
+		if(testInputUrl == null){
+			fail("Could not read resource file '" + INPUT_FILENAME + "'");
+			return;
+		}
+		
+		// Decode url to absolute format
+		String absoluteInputFilename = URLDecoder.decode(testInputUrl.getFile(), "UTF-8");
+		
+		// Read pages
+		parser.readPages(absoluteInputFilename);
 	}
 	
 	@Test
 	public void pagesMapShouldNotBeEmpty() throws XMLStreamException, IOException {
-		parser.readPages(absolutePath);
 		assertTrue("Pages map is empty.", parser.getRedirectedPages().size() > 0);
 	}
 	
 	@Test
-	public void pageDogShouldHaveThreeAlternativeTitle() throws XMLStreamException, IOException {
-		parser.readPages(absolutePath);
-		assertTrue("Page with title 'Dog' doesn't have three alternative title.", parser.getRedirectedPages().get("Dog").size() == 3);
+	public void pageComputeraAcessibilityShouldHaveTwoAlternativeTitles() throws XMLStreamException, IOException {
+		assertTrue("Page with title 'Computer accessibility' doesn't have two alternative titles.", parser.getRedirectedPages().get("Computer accessibility").size() == 2);
 	}
 	
 	@Test
-	public void pageFootballShouldHaveOneAlternativeTitleSoccer() throws XMLStreamException, IOException {
-		parser.readPages(absolutePath);
-		assertTrue("Page with title 'Football' doesn't have alternative title 'Soccer' (ignoring case).", parser.getRedirectedPages().get("Football").contains("Soccer"));
+	public void pageComputerAcessibilityShouldHaveAlternativeTitleAccessibleComputing() throws XMLStreamException, IOException {
+		assertTrue("Page with title 'ComputerAcessibility' doesn't have alternative title 'AccessibleComputing'.", parser.getRedirectedPages().get("Computer accessibility").contains("AccessibleComputing"));
 	}
 	
 	@Test
-	public void pageSoccerShouldNotHaveAlternativeTitles() throws XMLStreamException, IOException {
-		parser.readPages(absolutePath);
-		assertTrue("Page with title 'Soccer' shouldn't have alternative titles.", parser.getRedirectedPages().get("Soccer") != null);
+	public void pageRoadBicycleShouldHaveOneAlternativeTitle() throws XMLStreamException, IOException {
+		assertTrue("Page with title 'Road bicycle' doesn't have one alternative title.", parser.getRedirectedPages().get("Road bicycle").size() == 1);
+	}
+	
+	@Test
+	public void pageCarnaticShouldNotHaveAlternativeTitles() throws XMLStreamException, IOException {
+		assertTrue("Page with title 'Carnatic' shouldn't have alternative titles.", parser.getRedirectedPages().get("Carnatic").size() == 0);
+	}
+	
+	@Test
+	public void pageCortezShouldNotHaveAlternativeTitles() throws XMLStreamException, IOException {
+		assertTrue("Page with title 'Cortez' shouldn't have alternative titles.", parser.getRedirectedPages().get("Cortez").size() == 0);
 	}
 	
 	@Test
 	public void outputFileShouldBeCreated() throws IOException, XMLStreamException {
-		parser.readPages(absolutePath);
-
+		
 		// Save test output in default user home directory
-		String testOutputPath = FileSystemView.getFileSystemView().getHomeDirectory().getAbsolutePath() + "/test_output.xml";		
+		String testOutputPath = FileSystemView.getFileSystemView().getHomeDirectory().getAbsolutePath() + "/" + OUTPUT_FILENAME;		
 		parser.saveToCSV(testOutputPath);
 		
 		File f = new File(testOutputPath);
